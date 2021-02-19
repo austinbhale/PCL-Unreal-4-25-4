997a5246b8d08f0f61d844d8570f2734e5542c45
Austin Hale <haleau@live.unc.edu>
2021-02-19 18:52:49 -0500

    GPU Plugin files

---
+++ Plugins/UE4_GPUPointCloudRenderer/Source/GPUPointCloudRenderer/Private/IGPUPointCloudRenderer.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************************************
+* Written by Valentin Kraft <valentin.kraft@online.de>, http://www.valentinkraft.de, 2018
+**************************************************************************************************/
+
+#include "IGPUPointCloudRenderer.h"
+#include "CoreMinimal.h"
+#include "PointCloudStreamingCore.h"
+#include "Modules/ModuleManager.h"
+
+class FGPUPointCloudRendererPlugin : public IGPUPointCloudRenderer
+{
+
+	virtual void StartupModule() override
+	{
+	}
+
+	virtual void ShutdownModule() override
+	{
+	}
+
+	/**
+	* Returns a instance of the Point Cloud Core class.
+	*/
+	FPointCloudStreamingCore* CreateStreamingInstance(UMaterialInstanceDynamic* pointCloudShaderDynInstance) {
+		return new FPointCloudStreamingCore(pointCloudShaderDynInstance);
+	}
+};
+
+IMPLEMENT_MODULE(FGPUPointCloudRendererPlugin, GPUPointCloudRenderer)
